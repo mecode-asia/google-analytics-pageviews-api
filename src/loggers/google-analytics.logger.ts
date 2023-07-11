bc0bb4f1d5dab2a6b6fb456f8e525e87358c26b7
SPK137 <o.sippakorn@gmail.com>
2023-07-11 14:31:34 +0700

    refactor: use console logging instead of js-logger

--- src/loggers/google-analytics.logger.ts
+++ src/loggers/google-analytics.logger.ts
@@ -1,13 +1,12 @@
-import { IRunReportResponse } from '@src/typings';
-import Logger from 'js-logger';
+import { IRunReportResponse } from '../typings';
 
 export class GoogleAnalyticsLogger {
   constructor() {}
 
   async logPageviews(report: IRunReportResponse) {
-    Logger.info('Report result:');
+    console.info('Report result:');
     report.rows.forEach((row) => {
-      Logger.info(row.dimensionValues[0], row.metricValues[0]);
+      console.info(row.dimensionValues[0], row.metricValues[0]);
     });
   }
 }
