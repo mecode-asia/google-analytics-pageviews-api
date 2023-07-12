5f35947e1cfa28453844831366562d91cc913b39
SPK137 <o.sippakorn@gmail.com>
2023-07-12 11:53:38 +0700

    feat: add colors and prefix for logger

--- src/loggers/google-analytics.logger.ts
+++ src/loggers/google-analytics.logger.ts
@@ -1,37 +1,47 @@
 import { EnvError, IRunReportResponse } from '../typings';
-import Logger from 'js-logger';
+import clc from 'cli-color';
 
 export class GoogleAnalyticsLogger {
   constructor() {}
 
+  async warn(...text: unknown[]) {
+    console.warn(clc.yellow('WARNING:', text));
+  }
+
+  async info(...text: unknown[]) {
+    console.info(clc.blue(text));
+  }
+
   async logEnvError(envError: EnvError) {
     switch (envError) {
       case EnvError.MISSING_PROPERTY_ID:
-        Logger.warn(
+        this.warn(
           'Google Analytics Property ID is missing! Please input the ID when instantiating the GoogleAnalyticsClient.',
         );
         break;
       case EnvError.MISSING_CREDENTIALS_PATH:
-        Logger.warn(
+        this.warn(
           'Path to service account credentials file is missing! Please input the path when instantiating the GoogleAnalyticsClient.',
         );
         break;
       case EnvError.INCORRECT_CREDENTIALS_PATH:
-        Logger.warn(
+        this.warn(
           'Path to service account credentials file is incorrect! Please input the correct path when instantiating the GoogleAnalyticsClient.',
         );
         break;
       default:
-        Logger.warn(
+        this.warn(
           'There is an error due to misconfiguration of GA Pageviews API.',
         );
     }
   }
 
   async logPageviews(report: IRunReportResponse) {
-    Logger.info('Report result:');
+    this.info('Report result:');
     report.rows.forEach((row) => {
-      Logger.info(row.dimensionValues[0], row.metricValues[0]);
+      this.info(
+        `${row.dimensionValues[0].value} => ${row.metricValues[0].value}`,
+      );
     });
   }
 }
