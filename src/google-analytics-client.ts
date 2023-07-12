c1329f8b753fbcaaeae89eae68f673a246913579
SPK137 <o.sippakorn@gmail.com>
2023-07-12 10:40:37 +0700

    feat: add default value for env params

--- src/google-analytics-client.ts
+++ src/google-analytics-client.ts
@@ -3,7 +3,10 @@ import { GoogleAnalyticsService } from './services';
 
 export class GoogleAnalyticsClient {
   private gaService: GoogleAnalyticsService;
-  constructor(private gaPropertyID: string, private credentialsPath: string) {
+  constructor(
+    private gaPropertyID: string = '',
+    private credentialsPath: string = '',
+  ) {
     this.gaService = new GoogleAnalyticsService(this.gaPropertyID);
     process.env['GOOGLE_APPLICATION_CREDENTIALS'] = path.resolve(
       this.credentialsPath,
